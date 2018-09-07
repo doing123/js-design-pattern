8c6c65238da41b180fe9a500c5034c29622d5bf1
qinjunshan <13782533791@163.com>
2018-09-07 23:51:30 +0800

    publish Observer pattern

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-class Person {
-    constructor(name) {
-        this.name = name
+/* 观察者模式 */
+
+// 主题：保存状态，状态变化之后触发所有观察者对象
+class Subject {
+    constructor() {
+        this.state = 0;
+        this.observers = []
     }
-    getName() {
-        return this.name
+    getState() {
+        return this.state;
+    }
+    setState(state) {
+        this.state = state;
+        this.notifyAllObervers();
+    }
+    notifyAllObervers() {
+        this.observers.forEach(observe => {
+            observe.update();
+        })
+    }
+    attach(observer) {
+        this.observers.push(observer);
     }
 }
 
-let p = new Person('test')
+// 观察者
+class Observer {
+    constructor(name, subject) {
+        this.name = name;
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+    update() {
+        console.log(`${this.name} update, state: ${this.subject.getState()}`);
+    }
+}
 
-console.log(p)
+let s = new Subject()
+let o1 = new Observer('o1', s)
+s.setState(1)
